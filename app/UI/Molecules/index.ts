66a16384b3283a6c3dc470f26a95c0cb0a22beca
Manuel Garrido <m.garrido@quix.com.do>
2025-02-22 21:28:40 -0400

    Update Molecules index.ts

---
+++ app/UI/Molecules/index.ts
@@ -0,0 +1,20 @@
+export { default as DisplayDate } from "./DisplayDate";
+export type { IDisplayDateProps } from "./DisplayDate";
+
+export { default as NumericField } from "./NumericField";
+export type { INumericFieldProps } from "./NumericField";
+
+export { default as Card } from "./Card";
+export type { ICardProps } from "./Card";
+
+export { default as Snackbar } from "./Snackbar";
+export type { ISnackbarProps } from "./Snackbar";
+
+export { default as OverlayLoader } from "./OverlayLoader";
+export type { IOverlayLoaderProps } from "./OverlayLoader";
+
+export { default as SegmentedButtons } from "./SegmentedButtons";
+export type { ISegmentedButtonsProps } from "./SegmentedButtons";
+
+export { default as SearchInput } from "./SearchInput";
+export type { ISearchInputProps } from "./SearchInput";
