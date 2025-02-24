bb3b43b01982c844d76811878029cda75615725a
Manuel Garrido <m.garrido@quix.com.do>
2025-02-23 20:19:29 -0400

    Update Modules

--- app/Modules/Date/Date.ts
+++ app/Modules/Date/Date.ts
@@ -1,9 +1,14 @@
 import dayjs from "dayjs";
 import relativeTime from "dayjs/plugin/relativeTime";
 import localizedFormat from "dayjs/plugin/localizedFormat";
+import "dayjs/locale/es";
+import "dayjs/locale/en";
 
 dayjs.extend(relativeTime);
 dayjs.extend(localizedFormat);
+
+const userLocale = "es";
+dayjs.locale(userLocale);
 
 type DateParam = string | Date;
 
