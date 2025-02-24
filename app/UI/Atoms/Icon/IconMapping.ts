e6558437683b1bb699087f2625095e0bdcaaa0e6
Manuel Garrido <m.garrido@quix.com.do>
2025-02-24 08:45:33 -0400

    Update icons

--- app/UI/Atoms/Icon/IconMapping.ts
+++ app/UI/Atoms/Icon/IconMapping.ts
@@ -1,5 +1,11 @@
 import { StyledUtils } from "@/Utils";
-import { CaretLeft, MagnifyingGlass, Faders } from "phosphor-react-native";
+import {
+  CaretLeft,
+  MagnifyingGlass,
+  Faders,
+  House,
+  ArrowsLeftRight,
+} from "phosphor-react-native";
 
 import {
   LayoutProps,
@@ -22,6 +28,8 @@ const IconMapping = {
   Back: StyledUtils.styledIcon<TRestyleIconProps>(CaretLeft),
   Search: StyledUtils.styledIcon<TRestyleIconProps>(MagnifyingGlass),
   Filter: StyledUtils.styledIcon<TRestyleIconProps>(Faders),
+  Home: StyledUtils.styledIcon<TRestyleIconProps>(House),
+  Swap: StyledUtils.styledIcon<TRestyleIconProps>(ArrowsLeftRight),
 };
 
 export default IconMapping;
